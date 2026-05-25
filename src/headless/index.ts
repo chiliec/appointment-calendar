941907a36d39d1a2daa81b57d555218159849e4e
Vladimir Babin <vovababin@gmail.com>
2026-05-26 01:52:06 +0800

    feat: expose headless layer barrel + top-level entry

---
+++ src/headless/index.ts
@@ -0,0 +1,6 @@
+export { buildMonthGrid, formatYMD, timeToMinutes } from "./utils";
+export { hasConflict, findConflicts } from "./conflicts";
+export { useMonthGrid } from "./useMonthGrid";
+export type { UseMonthGridOptions, UseMonthGridReturn } from "./useMonthGrid";
+export { useDayTimeline } from "./useDayTimeline";
+export type { UseDayTimelineOptions, UseDayTimelineReturn } from "./useDayTimeline";
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
-export {};
+export * from "./headless";
+export type { Appointment, DayCounts, Labels } from "./types";
