347e0d55c306dfefecba7b6f54053b07b0997f0d
Vladimir Babin <vovababin@gmail.com>
2026-05-26 02:02:53 +0800

    feat: expose MonthCalendar and DayTimeline from styled barrel

--- src/styled/index.ts
+++ src/styled/index.ts
@@ -1,1 +1,4 @@
-export {};
+export { MonthCalendar } from "./MonthCalendar";
+export type { MonthCalendarProps } from "./MonthCalendar";
+export { DayTimeline } from "./DayTimeline";
+export type { DayTimelineProps } from "./DayTimeline";
